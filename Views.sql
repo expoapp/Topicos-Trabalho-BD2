6b128a17232070aab99d770c7857a50e7fe27d72
tvrl <baumilha10@hotmail.com>
2017-11-13 09:55:52 -0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,1 +1,7 @@
 
+  CREATE VIEW historico_notificao_empresa AS
+  SELECT empresa.nome, notificacao_id id_notificacao, notificacao.tipo, notificacao.tipo_origem, notificacao_descricao, notificacao.data
+  FROM empresa
+  INNER JOIN
+  on (empresa.id = notificacao.id_origem) and notificacao.id_origem = 1
+  ORDER BY empresa.nome
