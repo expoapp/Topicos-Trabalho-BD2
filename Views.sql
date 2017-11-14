a913bd9095d001e76582d713c9c846467419ea69
Yago Henrique <yagozt.henrique@gmail.com>
2017-11-13 22:21:06 -0200

    Update Views.sql
    View mostra notificação de exposições para o cliente

--- Views.sql
+++ Views.sql
@@ -1,3 +1,4 @@
+
 CREATE VIEW empresa_qtd_seguidores AS (
 SELECT empresa.nome, COUNT(*) quantidade_seguidores
 FROM empresa
@@ -28,3 +29,11 @@ INNER JOIN notificacao
 on empresa.id = notificacao.id_origem and notificacao.tipo_origem = 1
 ORDER BY empresa.nome
 );
+
+create view notificacao_exposicao_cliente as (
+select a.cpf,a.nome,d.descricao Exposicao ,c.descricao Notificacao ,c.data_cadastro from cliente a
+join cliente_notificacao b on a.id = b.fk_cliente_id
+join notificacao c on b.fk_notificacao_id = c.id
+join exposicao d on c.id_origem = d.id and c.tipo_origem = 2
+order by a.cpf, c.data_cadastro
+)
