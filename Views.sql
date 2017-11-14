4710c821a880af348399d9bd61c13de77d5a4d68
Yago Henrique <yagozt.henrique@gmail.com>
2017-11-13 22:08:24 -0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,29 +1,30 @@
-CREATE VIEW empresa_qtd_seguidores AS
+CREATE VIEW empresa_qtd_seguidores AS (
 SELECT empresa.nome, COUNT(*) quantidade_seguidores
 FROM empresa
-INNER JOIN cliente
-on (empresa.id = cliente.fk_empresa_id)
+INNER JOIN cliente_empresa
+on empresa.id = cliente_empresa.fk_empresa_id
 GROUP BY empresa.id
 ORDER BY quantidade_seguidores DESC
+  );
 
 
 
-CREATE VIEW cidade_qtd_empresas AS 
-SELECT estado.nome, COUNT(*) quantidade_empresas
-from cidade
-INNER JOIN 
-on (cidade.id = cidade.fk_cidade_id)
-INNER JOIN empresa
-on (bairro.id = empresa.fk_bairro_id)
+CREATE VIEW cidade_qtd_empresas AS (
+SELECT cidade.nome, COUNT(*) quantidade_empresas
+from empresa
+INNER JOIN bairro
+on empresa.fk_bairro_id = bairro.id
+INNER JOIN cidade
+on bairro.fk_cidade_id = cidade.id
 GROUP BY cidade.id
-ORDER BY quantidade_empresas   
+ORDER BY quantidade_empresas    
+);
   
   
-  
-CREATE VIEW historico_notificao_empresa AS
-SELECT empresa.nome, notificacao_id id_notificacao, notificacao.tipo, notificacao.tipo_origem, notificacao.descricao, notificacao.data
+create view historico_notificao_empresa as (
+SELECT empresa.nome, notificacao.id, notificacao.tipo, notificacao.tipo_origem, notificacao.descricao, notificacao.data_cadastro
 FROM empresa
-INNER JOIN
-on (empresa.id = notificacao.id_origem) and notificacao.id_origem = 1
+INNER JOIN notificacao
+on empresa.id = notificacao.id_origem and notificacao.tipo_origem = 1
 ORDER BY empresa.nome
-
+);
