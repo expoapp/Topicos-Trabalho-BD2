1027e1316fa90b3bd1dec5bb19aa53837db537b1
Yago Henrique <yagozt.henrique@gmail.com>
2017-11-20 09:29:05 -0200

    Update Indeces.sql

--- Indeces.sql
+++ Indeces.sql
@@ -1,1 +1,6 @@
-create index cliente_cpf on cliente using hash (cpf)
+
+create index cliente_cpf on cliente using hash (cpf);
+
+create index empresa_documento on empresa using hash (cnpj);
+
+create index notificacao_data on notificacao using btree (data_cadastro);
